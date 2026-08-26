ac9ad32f9fe9c3c39d5f212d71592b08c063370a
marymo <mo.mary.gabin@gmail.com>
2026-08-26 17:19:35 -0600

    ADD: backend game RESTful APIs

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,82 @@
+using GameStore.Api.Dtos;
+
+const string GetGameEndpoint = "GetGame";
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
+List<GameDto> games = [
+    new (
+        1, 
+        "Street Fighter II", 
+        "Fighting", 
+        19.99m, // m suffix indicates a decimal literal 
+        new DateOnly(1992, 7, 15)),
+    new (
+        2, 
+        "Final Fantasy VII Rebirth", 
+        "RPG", 
+        69.99m, // m suffix indicates a decimal literal 
+        new DateOnly(2024, 2, 29)),
+    new (
+        3, 
+        "Astro Bot", 
+        "Platformer", 
+        59.99m, // m suffix indicates a decimal literal 
+        new DateOnly(2024, 9, 06)),
+        
+];
+
 app.MapGet("/", () => "Hello World!");
+
+// GET /games
+app.MapGet("/games", () => games);
+
+// GET /games/{id}
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
+    .WithName(GetGameEndpoint);
+
+// POST /games
+app.MapPost("/games", (CreateGameDto newGame) =>
+{
+   GameDto game = new(
+    games.Count + 1,
+    newGame.Name,
+    newGame.Genre,
+    newGame.Price,
+    newGame.ReleaseDate
+   );
+
+   games.Add(game);
+
+   return Results.CreatedAtRoute(GetGameEndpoint, new {id = game.Id}, game); 
+});
+
+// PUT requests should be thread-safe to prevent data inconsistencies; not implemented here yet
+// PUT /games/1
+app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
+{
+    var index = games.FindIndex(game => game.Id == id);
+
+    games[index] = new GameDto(
+        id,
+        updatedGame.Name,
+        updatedGame.Genre,
+        updatedGame.Price,
+        updatedGame.ReleaseDate
+    );
+
+    return Results.NoContent();
+});
+
+// DELETE /games/1
+app.MapDelete("/games/{id}", (int id) =>
+{
+    games.RemoveAll(game => game.Id == id);
+
+    return Results.NoContent(); 
+});
+
 
 app.Run();
