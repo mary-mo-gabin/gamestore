dacf984324c2869cd49f77f52db155febf192512
marymo <mo.mary.gabin@gmail.com>
2026-08-27 16:47:36 -0600

    Add: non-existent resource handling for GET and PUT

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -33,9 +33,14 @@ app.MapGet("/", () => "Hello World!");
 // GET /games
 app.MapGet("/games", () => games);
 
-// GET /games/{id}
-app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
-    .WithName(GetGameEndpoint);
+// GET /games/1
+app.MapGet("/games/{id}", (int id) => 
+{
+    var game = games.Find(game => game.Id == id);
+
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
+.WithName(GetGameEndpoint);
 
 // POST /games
 app.MapPost("/games", (CreateGameDto newGame) =>
@@ -58,6 +63,11 @@ app.MapPost("/games", (CreateGameDto newGame) =>
 app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
 {
     var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1) // if game not found
+    {
+        return Results.NotFound();
+    }
 
     games[index] = new GameDto(
         id,
