5b3e50ec38ebd0590ff7b72946e8db2ddfb3e21a
marymo <mo.mary.gabin@gmail.com>
2026-08-29 02:32:55 -0600

    feat(db): implement initial data seeding on application startup as a data extension method

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,11 +1,13 @@
 // to allow for migrations to be applied on startup
 
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
 
 public static class DataExtensions
 {
+    // migrate the database on startup
     public static void MigrateDB(this WebApplication app)
     {
         using var scope = app.Services.CreateScope(); // needed for dependency injection
@@ -13,5 +15,29 @@ public static class DataExtensions
         var dbContext = scope.ServiceProvider
                             .GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+
+    // seed the database on startup
+    public static void AddGameStoreDb(this WebApplicationBuilder builder)
+    {
+        var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(
+    connString,
+    optionsAction: options => options.UseSeeding((context, _) =>
+    {
+        if (!context.Set<Genre>().Any())
+        {
+            context.Set<Genre>().AddRange(
+                new Genre { Name = "Fighting" },
+                new Genre { Name = "RPG" },
+                new Genre { Name = "Platformer" },
+                new Genre { Name = "Racing" },
+                new Genre { Name = "Sports" }
+            );
+
+            context.SaveChanges();
+        }
+    })
+);
     }
 }
