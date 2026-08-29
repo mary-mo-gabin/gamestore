d30d62f7e7e52402601b3398a093c4e90679eb42
marymo <mo.mary.gabin@gmail.com>
2026-08-29 02:38:15 -0600

    fix: move connection string to appsettings.json config file

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -20,24 +20,24 @@ public static class DataExtensions
     // seed the database on startup
     public static void AddGameStoreDb(this WebApplicationBuilder builder)
     {
-        var connString = "Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(
-    connString,
-    optionsAction: options => options.UseSeeding((context, _) =>
-    {
-        if (!context.Set<Genre>().Any())
-        {
-            context.Set<Genre>().AddRange(
-                new Genre { Name = "Fighting" },
-                new Genre { Name = "RPG" },
-                new Genre { Name = "Platformer" },
-                new Genre { Name = "Racing" },
-                new Genre { Name = "Sports" }
-            );
+        var connString = builder.Configuration.GetConnectionString("GameStore");
+        builder.Services.AddSqlite<GameStoreContext>(
+            connString,
+            optionsAction: options => options.UseSeeding((context, _) =>
+            {
+                if (!context.Set<Genre>().Any())
+                {
+                    context.Set<Genre>().AddRange(
+                        new Genre { Name = "Fighting" },
+                        new Genre { Name = "RPG" },
+                        new Genre { Name = "Platformer" },
+                        new Genre { Name = "Racing" },
+                        new Genre { Name = "Sports" }
+                    );
 
-            context.SaveChanges();
-        }
-    })
-);
+                    context.SaveChanges();
+                }
+            })
+        );
     }
 }
